319316a031110a9af83e1214857a6daed972501c
mertdlkr <mertadalkir@gmail.com>
2025-08-17 19:50:44 +0300

    vercel5

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  experimental: {
+    typedRoutes: true,
+  },
 };
 
 export default nextConfig;
